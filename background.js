3bfaf06bf170e41fda71196214ddecbc759953d0
hrishiksh <hrishi1pathak@gmail.com>
2023-02-17 11:43:14 +0530

    scihub url changed

--- background.js
+++ background.js
@@ -4,16 +4,50 @@ chrome.runtime.onInstalled.addListener(() => {
     contexts: ["selection", "link"],
     id: "openwithscholarr",
   });
+  // chrome.contextMenus.create({
+  //   title: "Copy APA citation",
+  //   contexts: ["selection", "link"],
+  //   id: "copycitation",
+  // });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  let constructedUrl = `https://sci-hub.se/${
-    info.linkUrl ? info.linkUrl : info.selectionText
-  }`;
+  if (info.menuItemId == "openwithscholarr") {
+    let constructedUrl = `https://sci-hub.ru/${
+      info.linkUrl ? info.linkUrl : info.selectionText
+    }`;
 
-  console.log(info);
-  console.log(constructedUrl);
-  chrome.tabs.create({
-    url: constructedUrl,
-  });
+    console.log(info);
+    console.log(constructedUrl);
+    chrome.tabs.create({
+      url: constructedUrl,
+    });
+  }
 });
+
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//   if (info.menuItemId == "copycitation") {
+//     console.log(document.execCommand("paste"));
+
+//     // let doiLink = info.linkUrl;
+//     // fetch(
+//     //   `https://citation.crosscite.org/format?doi=${doiLink}&style=apa&lang=en-US`
+//     // )
+//     //   .then((response) => response.text())
+//     //   .then((response) => {
+//     //     console.log(response);
+//     //     navigator.clipboard.writeText("hi");
+//     //   })
+//     //   .catch((err) => console.error(err));
+//   } else {
+//     let constructedUrl = `https://sci-hub.se/${
+//       info.linkUrl ? info.linkUrl : info.selectionText
+//     }`;
+
+//     console.log(info);
+//     console.log(constructedUrl);
+//     chrome.tabs.create({
+//       url: constructedUrl,
+//     });
+//   }
+// });
